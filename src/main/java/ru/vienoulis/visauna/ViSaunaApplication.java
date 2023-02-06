89215543d30c309c2d23faafdc22cebdd0ed6c97
lyk <roman.lukyanov@x5.ru>
2023-02-06 17:00:20 +0300

    first

---
+++ src/main/java/ru/vienoulis/visauna/ViSaunaApplication.java
@@ -0,0 +1,13 @@
+package ru.vienoulis.visauna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViSaunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViSaunaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vienoulis/visauna/ViSaunaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vienoulis.visauna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViSaunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
