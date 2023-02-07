166f74ffe2e956bdcfdc14ccc32d348527d53610
lyk <roman.lukyanov@x5.ru>
2023-02-07 10:44:56 +0300

    restart после заливки на сервер

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 scp build/libs/ViSauna-0.0.1.jar root@94.198.220.140:/root/viSauna/
 scp src/main/resources/application.yaml root@94.198.220.140:/root/viSauna/
+ssh root@94.198.220.140 systemctl restart viSauna.service
